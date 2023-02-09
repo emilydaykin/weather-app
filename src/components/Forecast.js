53673fe65ff797912e01ddb7e16bc97568289b18
Emily Daykin <emilydaykin31@gmail.com>
2023-02-09 09:21:45 +0100

    clean up and populated the rest of the months in forecast

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,9 +1,20 @@
 import Image from "next/image";
 
 const Forecast = ({ cityWeatherResponse }) => {
-  console.log("cityWeatherRepsonse FROM FORECAST COMPONENT", cityWeatherResponse);
-
-  const translateMonth = { 1: "Jan", 2: "Feb" };
+  const translateMonth = {
+    1: "Jan",
+    2: "Feb",
+    3: "Mar",
+    4: "Apr",
+    5: "May",
+    6: "Jun",
+    7: "Jul",
+    8: "Aug",
+    9: "Sep",
+    10: "Oct",
+    11: "Nov",
+    12: "Dec",
+  };
 
   const formatDate = (date) => {
     const dayInt = parseInt(date.split("-")[2]);
